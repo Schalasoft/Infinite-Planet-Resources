b49c9f4564ecb04ff4afa839744d7136aa83593f
Cian <47340380+Schalasoft@users.noreply.github.com>
2019-08-08 16:21:35 +0100

    edit

---
+++ InfinitePlanetResources/InfinitePlanetResources.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+
+namespace InfinitePlanetResources
+{
+    [HarmonyPatch(typeof(SpaceDestination), "Replenish")]
+    public class SpaceDestination_Replenish_Patch : SpaceDestination
+    {
+        public SpaceDestination_Replenish_Patch(int id, string type, int distance) : base(id, type, distance)
+        {
+        }
+
+        public static void Postfix(SpaceDestination __instance)
+        {
+            FieldInfo availableMass_Field = __instance.GetType().GetField("availableMass", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (availableMass_Field != null) availableMass_Field.SetValue(__instance, (float)__instance.GetDestinationType().maxiumMass);
+        }
+     }
+    
+}
